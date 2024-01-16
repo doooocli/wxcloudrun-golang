6b9b9786339108d815731194e316ad51958e4a70
Doc <doooocli@protonmail.com>
2024-01-16 12:26:03 +0800

    test

--- conf/conf.go
+++ conf/conf.go
@@ -1,1 +1,6 @@
 package conf
+
+const (
+	AppId     = "wxe63b6755f985eb15"
+	AppSecret = "11c616d402f4ad2c466947ac57e19151"
+)
--- service/marketcode_service.go
+++ service/marketcode_service.go
@@ -1,60 +1,18 @@
 package service
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
-	"time"
-
-	"wxcloudrun-golang/db/dao"
-	"wxcloudrun-golang/db/model"
-
-	"gorm.io/gorm"
+	"wxcloudrun-golang/conf"
 )
 
-// JsonResult 返回结构
-type JsonResult struct {
-	Code     int         `json:"code"`
-	ErrorMsg string      `json:"errorMsg,omitempty"`
-	Data     interface{} `json:"data"`
-}
-
-// IndexHandler 计数器接口
-func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	data, err := getIndex()
-	if err != nil {
-		fmt.Fprint(w, "内部错误")
-		return
-	}
-	fmt.Fprint(w, data)
-}
-
-// CounterHandler 计数器接口
-func CounterHandler(w http.ResponseWriter, r *http.Request) {
+// 申请二维码
+func ApplycodeHandler(w http.ResponseWriter, r *http.Request) {
 	res := &JsonResult{}
-
-	if r.Method == http.MethodGet {
-		counter, err := getCurrentCounter()
-		if err != nil {
-			res.Code = -1
-			res.ErrorMsg = err.Error()
-		} else {
-			res.Data = counter.Count
-		}
-	} else if r.Method == http.MethodPost {
-		count, err := modifyCounter(r)
-		if err != nil {
-			res.Code = -1
-			res.ErrorMsg = err.Error()
-		} else {
-			res.Data = count
-		}
-	} else {
-		res.Code = -1
-		res.ErrorMsg = fmt.Sprintf("请求方法 %s 不支持", r.Method)
-	}
-
+	res.Data, _ = getStableAccessToken()
 	msg, err := json.Marshal(res)
 	if err != nil {
 		fmt.Fprint(w, "内部错误")
@@ -64,96 +22,32 @@ func CounterHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write(msg)
 }
 
-// modifyCounter 更新计数，自增或者清零
-func modifyCounter(r *http.Request) (int32, error) {
-	action, err := getAction(r)
-	if err != nil {
-		return 0, err
-	}
-
-	var count int32
-	if action == "inc" {
-		count, err = upsertCounter(r)
-		if err != nil {
-			return 0, err
-		}
-	} else if action == "clear" {
-		err = clearCounter()
-		if err != nil {
-			return 0, err
-		}
-		count = 0
-	} else {
-		err = fmt.Errorf("参数 action : %s 错误", action)
-	}
-
-	return count, err
+type AccessTokenResult struct {
+	ErrCode     int    `json:"errcode,omitempty"`
+	ErrMsg      string `json:"errmsg,omitempty"`
+	AccessToken string `json:"access_token,omitempty"`
+	ExpiresIn   int    `json:"expires_in,omitempty"`
 }
 
-// upsertCounter 更新或修改计数器
-func upsertCounter(r *http.Request) (int32, error) {
-	currentCounter, err := getCurrentCounter()
-	var count int32
-	createdAt := time.Now()
-	if err != nil && err != gorm.ErrRecordNotFound {
-		return 0, err
-	} else if err == gorm.ErrRecordNotFound {
-		count = 1
-		createdAt = time.Now()
-	} else {
-		count = currentCounter.Count + 1
-		createdAt = currentCounter.CreatedAt
-	}
+// getStableAccessToken 获取稳定版接口调用凭据
+func getStableAccessToken() (*AccessTokenResult, error) {
 
-	counter := &model.CounterModel{
-		Id:        1,
-		Count:     count,
-		CreatedAt: createdAt,
-		UpdatedAt: time.Now(),
-	}
-	err = dao.Imp.UpsertCounter(counter)
-	if err != nil {
-		return 0, err
-	}
-	return counter.Count, nil
-}
-
-func clearCounter() error {
-	return dao.Imp.ClearCounter(1)
-}
-
-// getCurrentCounter 查询当前计数器
-func getCurrentCounter() (*model.CounterModel, error) {
-	counter, err := dao.Imp.GetCounter(1)
+	jsonStr := []byte(`{ "grant_type": "client_credential", "appid": "` + conf.AppId + `", "secret": "` + conf.AppSecret + `" }`)
+	resp, err := http.Post("https://api.weixin.qq.com/cgi-bin/stable_token", "application/json", bytes.NewBuffer(jsonStr))
 	if err != nil {
 		return nil, err
 	}
-
-	return counter, nil
-}
-
-// getAction 获取action
-func getAction(r *http.Request) (string, error) {
-	decoder := json.NewDecoder(r.Body)
-	body := make(map[string]interface{})
-	if err := decoder.Decode(&body); err != nil {
-		return "", err
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("%s", resp.Status)
 	}
-	defer r.Body.Close()
-
-	action, ok := body["action"]
-	if !ok {
-		return "", fmt.Errorf("缺少 action 参数")
-	}
-
-	return action.(string), nil
-}
-
-// getIndex 获取主页
-func getIndex() (string, error) {
-	b, err := ioutil.ReadFile("./index.html")
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	return string(b), nil
+	resp.Body.Close()
+	result := &AccessTokenResult{}
+	if err := json.Unmarshal(body, result); err != nil {
+		return nil, err
+	}
+	return result, nil
 }
